cb0bce03bd9b4d95d74946da93cb1b98982eb35e
Khalil Tourabi <ktourabi@gmail.com>
2024-06-01 20:07:02 +0100

    user route and controller add

---
+++ routes/UserRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {updateUser, deleteUser, getUser,} from "../controllers/UserController.js";
+import tokenVerification from "../middlewares/tokenVerification.js";
+
+const userRoute = express.Router();
+
+userRoute.put("/updateUser", tokenVerification, updateUser).get("/getUser", tokenVerification, getUser).delete("/deleteUser", tokenVerification, deleteUser);
+
+export default userRoute;
